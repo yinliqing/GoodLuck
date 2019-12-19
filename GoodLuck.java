ebc8f6b5b52d5e2e3af6fd9db77d0f931bef7bcb
yinliqing <yinliqing_md@sina.com>
2019-12-19 11:31:44 +0800

    修改提示信息

--- GoodLuck.java
+++ GoodLuck.java
@@ -2,10 +2,8 @@
 public class GoodLuck {
 
 	public static void main(String[] args) {
-		System.out.println("Gook Luck");
+		System.out.println("Gook Luck1 ok");
 		System.out.println("Gook Luck2");
-		System.out.println("Gook Luck3");
-		System.out.println("Gook Luck4");
 	}
 	
 }
