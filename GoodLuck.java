f5bddda91bd61f089ef3c8df971fbfe6b220e923
yinliqing <yinliqing_md@sina.com>
2023-09-20 10:42:29 +0800

    Update GoodLuck.java
    2023.09.20

--- GoodLuck.java
+++ GoodLuck.java
@@ -3,7 +3,7 @@ public class GoodLuck {
 
 	public static void main(String[] args) {
 		System.out.println("Gook Luck1 ok");
-		System.out.println("Gook Luck2 ok 2021.12.02");
+		System.out.println("Gook Luck2 ok 2023.09.20");
 	}
 	
 }
